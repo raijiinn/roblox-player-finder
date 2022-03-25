6286ac4529edb5495ab87ec02126816a84a3453b
Rayan <99978591+raijiinn@users.noreply.github.com>
2022-03-25 21:11:34 +0100

    Update findplayer.py

--- findplayer.py
+++ findplayer.py
@@ -1,9 +1,10 @@
 import requests
+import time
 import threading
-userid = '152248486'
-placeid = '7180042682'
+userid = '1088819236'
+placeid = '7115420363'
 Found = False
-cookie =  "enter your cookie here"
+cookie =  "enter ROBLOSECURITY cookie here"
 cookies = {".ROBLOSECURITY":cookie} # set cookie dictionary/json
 image = requests.get(f"https://www.roblox.com/headshot-thumbnail/image?userId={userid}&width=48&height=48&format=png").url # get user image
 #xheaderthing = requests.post(f"https://auth.roblox.com/v2/signup",cookies=cookies).headers["x-csrf-token"] # get the x-csrf-token header
@@ -14,14 +15,16 @@ image = requests.get(f"https://www.roblox.com/headshot-thumbnail/image?userId={u
 def findplayer(placeid,idx):
    global Found
    for i in range(10): # 10 retries if ratelimited etc. 
-    try:       
-        print(idx)
+    try:   
+        print(idx)    
         serverindex = requests.get(f'https://www.roblox.com/games/getgameinstancesjson?placeId={placeid}&startIndex={str(idx)}',cookies=cookies)  # get server     
         serversize = serverindex.json()['TotalCollectionSize']
-        currentsize = len(serverindex.json()['Collection'])
+        currentsize = len(serverindex.json()['Collection'])      
         for i in range(11): # this is for the last servers they dont have 10 servers in the list so we check if we actually hit the end by looping through it 10 times and seeing if we get any lists with data if not then it will return found or it will end 
             if currentsize < 10: # checks size of list
-                serverindex = requests.get(f'https://www.roblox.com/games/getgameinstancesjson?placeId={placeid}&startIndex={str(idx-i)}',cookies=cookies)   # sets back index             
+                serverindex = requests.get(f'https://www.roblox.com/games/getgameinstancesjson?placeId={placeid}&startIndex={str(idx-i)}',cookies=cookies)   # sets back index   
+                if len(serverindex.json()['Collection']) > 0:
+                    break   
         for servers in serverindex.json()['Collection']:
             for players in servers['CurrentPlayers']:
                 if players['Thumbnail']['Url'] == image:                
@@ -35,7 +38,6 @@ def findplayer(placeid,idx):
 
     except Exception as ex:
         print(ex)    
-    
 serversize = findplayer(placeid,1) # gets length of all servers
 for i in range(0,serversize,10): # loops through all servers with a increment of 10
     if Found == True:
